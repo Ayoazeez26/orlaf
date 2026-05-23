413baf4bd297616c11a626c998eaa4b2f5a17ba1
Adeyemi Gbenga <addegbenga@gmail.com>
2026-05-23 10:19:13 +0100

    feat: added sable package

--- apps/creator-web/src/routes/dashboard/index.tsx
+++ apps/creator-web/src/routes/dashboard/index.tsx
@@ -2,9 +2,9 @@ import { createFileRoute } from "@tanstack/react-router"
 import { HomePage } from "@/features/dashboard/components/home/home-page"
 
 export const Route = createFileRoute("/dashboard/")({
-  component: Dashboard
+  component: Dashboard,
 })
 
-function Dashboard (){
-  return < HomePage/>
+function Dashboard() {
+  return <HomePage />
 }
